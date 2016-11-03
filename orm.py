c15dc4f6fa0b3b1d7c2c830e8bc171ab5c85efb5
hexinzhe <hexinzhe@live.com>
2016-11-03 19:57:47 +0800

    fix orm.py issues

--- orm.py
+++ orm.py
@@ -2,7 +2,73 @@ import logging; logging.basicConfig(level=logging.INFO)
 import aiomysql
 
 
-class Model(dict):
+# ORM中最重要的东西，元类。创建类对象的类，可谓之类中之类
+class ModelMetaclass(type):
+    # 四个参数依次是cls：类的对象（type类型），name：类名，bases：基类（数组类型），attrs：类中所有属性和方法
+    def __new__(cls, name, bases, attrs):
+        # 排除Model类本身,只处理用户自定义类
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)
+
+        # 获取table名.如果没定义__table__属性，则默认使用类名
+        tableName = attrs.get('__table__', name)
+        logging.info('found model: %s (table: %s)' % (name, tableName))
+
+        # 获取所有的Field和主键名
+        mappings = dict()
+        fields = []
+        primaryKey = None
+
+        # 迭代所有属性和方法，保存所有Field属性和主键，用于SQL语句的映射
+        # 在这里，k 是属性名或方法名，v 是 k 所代表的对象
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info('  found mapping: %s ==> %s' % (k, v))
+                # 此处的 v 是Field对象
+                mappings[k] = v
+
+                # 查找主键
+                if v.primary_key:
+                    # 找到主键
+                    if primaryKey:
+                        # 如果已经存在一个主键
+                        raise RuntimeError('Duplicate primary key for field: %s' % k)
+
+                    # 否则将 k 赋给primaryKey
+                    primaryKey = k
+                else:
+                    # 如果不是主键，则将其加进数组，这里保存key，用于SQL语句
+                    fields.append(k)
+
+        # 迭代完成，如果没有找到主键
+        if not primaryKey:
+            raise RuntimeError('Primary key not found.')
+        # 黑魔法，删掉所有的Field类属性，为了避免属性冲突。它们定义映射关系的目的已经在上面完成了，现在没用了，所以删掉
+        for k in mappings.keys():
+            attrs.pop(k)
+
+        # 将fields转义,注意后面的lambda的结束位置，到第二个f为止，fields是map的第二个参数
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings  # 保存属性和列的映射关系
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey  # 主键属性名
+        attrs['__fields__'] = fields  # 除主键外的属性名
+
+        # 构造默认的SELECT, INSERT, UPDATE, DELETE语句：
+        attrs['__select__'] = 'select * from `%s`' % tableName
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) VALUES (%s)' % (tableName,
+                                                                           ', '.join(escaped_fields),
+                                                                           primaryKey,
+                                                                           create_args_string(len(fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s WHERE `%s`=?' % \
+                              (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` WHERE `%s`=?' % (tableName, primaryKey)
+
+        # 好长的一个函数
+        return type.__new__(cls, name, bases, attrs)
+
+
+class Model(dict, metaclass=ModelMetaclass):
     def __init__(self, **kw):
         super(Model, self).__init__(**kw)
 
@@ -22,7 +88,7 @@ class Model(dict):
     def getValueOrDefault(self, key):
         value = getattr(self, key, None)
         if value is None:
-            field = self.__mapings__[key]
+            field = self.__mappings__[key]
             if field.default is not None:
                 value = field.default() if callable(field.default) else field.default
                 logging.debug('using default value for %s: %s' % (key, str(value)))
@@ -119,69 +185,6 @@ class Model(dict):
             logging.warning('failed to remove by primary key: affected rows: %s' % rows)
 
 
-# ORM中最重要的东西，元类。创建类对象的类，可谓之类中之类
-class ModelMetaclass(type):
-    # 四个参数依次是cls：类的对象（type类型），name：类名，bases：基类（数组类型），attrs：类中所有属性和方法
-    def __new__(cls, name, bases, attrs):
-        # 排除Model类本身,只处理用户自定义类
-        if name == 'Model':
-            return type.__new__(cls, name, bases, attrs)
-
-        # 获取table名.如果没定义__table__属性，则默认使用类名
-        tableName = attrs.get('__table__', name)
-        logging.info('found model: %s (table: %s)' % (name, tableName))
-
-        # 获取所有的Field和主键名
-        mappings = dict()
-        fields = []
-        primaryKey = None
-
-        # 迭代所有属性和方法，保存所有Field属性和主键，用于SQL语句的映射
-        # 在这里，k 是属性名或方法名，v 是 k 所代表的对象
-        for k, v in attrs.items():
-            if isinstance(v, Field):
-                logging.info('  found mapping: %s ==> %s' % (k, v))
-                # 此处的 v 是Field对象
-                mappings[k] = v
-
-                # 查找主键
-                if v.primary_key:
-                    # 找到主键
-                    if primaryKey:
-                        # 如果已经存在一个主键
-                        raise RuntimeError('Duplicate primary key for field: %s' % k)
-
-                    # 否则将 k 赋给primaryKey
-                    primaryKey = k
-                else:
-                    # 如果不是主键，则将其加进数组，这里保存key，用于SQL语句
-                    fields.append(k)
-
-        # 迭代完成，如果没有找到主键
-        if not primaryKey:
-            raise RuntimeError('Primary key not found.')
-        # 黑魔法，删掉所有的Field类属性，为了避免属性冲突。它们定义映射关系的目的已经在上面完成了，现在没用了，所以删掉
-        for k in mappings.keys():
-            attrs.pop(k)
-
-        # 将fields转义,注意后面的lambda的结束位置，到第二个f为止，fields是map的第二个参数
-        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
-        attrs['__mappings__'] = mappings  # 保存属性和列的映射关系
-        attrs['__table__'] = tableName
-        attrs['__primary_key__'] = primaryKey  # 主键属性名
-        attrs['__fields__'] = fields  # 除主键外的属性名
-
-        # 构造默认的SELECT, INSERT, UPDATE, DELETE语句：
-        attrs['__select__'] = 'select * from `%s`' % tableName
-        attrs['__insert__'] = 'insert into `%s` VALUES (%s)' % (tableName, create_args_string(len(fields) + 1))
-        attrs['__update__'] = 'update `%s` set %s WHERE `%s`=?' %\
-                              (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
-        attrs['__delete__'] = 'delete from `%s` WHERE `%s`=?' % (tableName, primaryKey)
-
-        # 好长的一个函数
-        return type.__new__(cls, name, bases, attrs)
-
-
 # 为各种类型映射提供关系
 class Field(object):
     def __init__(self, name, column_type, primary_key, default):
@@ -230,7 +233,7 @@ async def create_pool(loop, **kw):
     global __pool
     # 创建连接池
     __pool = await aiomysql.create_pool(
-        host=kw.get('host', 'localhost'),
+        host=kw.get('host', '127.0.0.1'),
         port=kw.get('port', 3306),
         user=kw['user'],
         password=kw['password'],
@@ -242,6 +245,12 @@ async def create_pool(loop, **kw):
         loop=loop
 
     )
+
+async def close_pool():
+    global __pool
+    if __pool:
+        __pool.close()
+        await __pool.wait_closed()
 
 # 映射select语句
 async def select(sql, args, size=None):
@@ -286,8 +295,8 @@ async def execute(sql, args):
 
 # 用于输出格式化的SQL执行语句
 def log(sql, args=()):
-    escape_sql = sql.replace('?', '%s')
-    logging.info('SQL: %s' % escape_sql, args)
+    # escape_sql = sql.replace('?', '%s')
+    logging.info('SQL: %s' % sql)
 
 
 # 用于执行insert语句时创建参数占位'?'，因为insert语句参数较多，所以封装一个函数。有几个列属性，就创建几个占位符
