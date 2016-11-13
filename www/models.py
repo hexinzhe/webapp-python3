e9964ed64253d3a873b5d6ce7423bce218ef1b31
hexinzhe <hexinzhe@live.com>
2016-11-13 23:06:11 +0800

    'fix models issues'

--- www/models.py
+++ www/models.py
@@ -10,24 +10,24 @@ def next_id():
 class User(Model):
     __table__ = 'users'
 
-    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-    email = StringField(ddl='varchar(50)')
-    passwd = StringField(ddl='varchar(50)')
+    id = StringField(primary_key=True, default=next_id, map_type='varchar(50)')
+    email = StringField(map_type='varchar(50)')
+    passwd = StringField(map_type='varchar(50)')
     admin = BooleanField()
-    name = StringField(ddl='varchar(50)')
-    image = StringField(ddl='varchar(500)')
+    name = StringField(map_type='varchar(50)')
+    image = StringField(map_type='varchar(500)')
     created_at = FloatField(default=time.time)
 
 
 class Blog(Model):
     __table__ = 'blogs'
 
-    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-    user_id = StringField(ddl='varchar(50)')
-    user_name = StringField(ddl='varchar(50)')
-    user_image = StringField(ddl='varchar(500)')
-    name = StringField(ddl='varchar(50)')
-    summary = StringField(ddl='varchar(200)')
+    id = StringField(primary_key=True, default=next_id, map_type='varchar(50)')
+    user_id = StringField(map_type='varchar(50)')
+    user_name = StringField(map_type='varchar(50)')
+    user_image = StringField(map_type='varchar(500)')
+    name = StringField(map_type='varchar(50)')
+    summary = StringField(map_type='varchar(200)')
     content = TextField()
     created_at = FloatField(default=time.time)
 
@@ -35,10 +35,10 @@ class Blog(Model):
 class Comment(Model):
     __table__ = 'comments'
 
-    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-    blog_id = StringField(ddl='varchar(50)')
-    user_id = StringField(ddl='varchar(50)')
-    user_name = StringField(ddl='varchar(50)')
-    user_image = StringField(ddl='varchar(500)')
+    id = StringField(primary_key=True, default=next_id, map_type='varchar(50)')
+    blog_id = StringField(map_type='varchar(50)')
+    user_id = StringField(map_type='varchar(50)')
+    user_name = StringField(map_type='varchar(50)')
+    user_image = StringField(map_type='varchar(500)')
     content = TextField()
     created_at = FloatField(default=time.time)
